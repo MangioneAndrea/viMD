f0c088fe12374af2a4557da7362730f1cb9a1f67
Andrea Mangione <andrea@mangione.dev>
2023-09-16 21:13:12 +0200

    feat: finish up draggable

--- src/components/draggable-separator.tsx
+++ src/components/draggable-separator.tsx
@@ -3,20 +3,24 @@ import { Component, JSXElement, createSignal } from 'solid-js';
 export const DraggableSeparator: Component<{
     children: [JSXElement, JSXElement];
 }> = ({ children }) => {
-    const [ratio, setRatio] = createSignal([0.5, 0.5]);
+    const [ratio, setRatio] = createSignal([0.4, 0.6]);
     const drag = (evt: DragEvent) => {
-        console.log(evt.x, evt.target.parentNode.offsetWidth);
+        evt.preventDefault();
+        if (evt.screenX === 0) return;
+        const first = evt.x / evt.target.parentNode.offsetWidth;
+
+        setRatio([first, 1 - first]);
     };
 
     return (
         <div class="flex w-full">
-            <div class="flex-1">{children[0]}</div>
+            <div style={`flex: ${ratio()[0]}`}>{children[0]}</div>
             <span
                 class="bg-gray-600 w-1 h-full cursor-col-resize"
                 onDrag={drag}
                 draggable
             />
-            <div class="flex-1">{children[1]}</div>
+            <div style={`flex: ${ratio()[1]}`}>{children[1]}</div>
         </div>
     );
 };
